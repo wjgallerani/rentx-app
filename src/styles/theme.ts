9cbf74dca599cedbdb869f030de3f772a632ec70
William Jose Gallerani <wjgallerani@saomanoel.com.br>
2022-02-01 23:34:50 -0300

    Atualização

---
+++ src/styles/theme.ts
@@ -0,0 +1,29 @@
+export default {
+    colors: {
+        header: '#1B1B1F',
+
+        background_primary: '#F5F4F6',
+        background_secundary: '#FFFFFF',
+
+        text: '#7A7A80',
+        text_detail: '#AEAEB3',
+        title: '#47474D',
+
+        line: '#EBEBF0',
+
+        main: '#DC1637',
+        main_ligth: '#FDEDEF',
+        success: '#03B252',
+
+        shape: '#E1E1E8',
+        shape_dart: '#29292E'
+    },
+    fonts: {
+        primary_400: 'Inter_400Regular',
+        primary_500: 'Inter_500Medium',
+
+        secondary_400: 'Archivo_400Regular',
+        secondary_500: 'Archivo_500Medium',
+        secondary_600: 'Archivo_600SemiBol'
+    }
+}
